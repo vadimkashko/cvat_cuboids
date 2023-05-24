204903e74e9b8605f10638251d71a83e42ebbf7d
Vadzim <75391509+vadimkashko@users.noreply.github.com>
2023-05-24 05:58:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,40 +6,75 @@ from environs import Env
 from PIL import Image, ImageDraw
 
 
+def calc_fourth_point(*args):
+    a, b, c = args
+    o = [(a[0] + c[0]) / 2, (a[1] + c[1]) / 2]
+    d = (2 * o[0] - b[0], 2 * o[1] - b[1])
+
+    return d
+
+
 def draw_shapes(image: Image.Image, annotations: dict) -> Image.Image:
     image = image
     draw = ImageDraw.Draw(image)
+
+    line_width = 1
+    if image.width * image.height >= 1000000:
+        line_width = 2
+    elif image.width * image.height >= 3000000:
+        line_width = 3
+
     for shape in annotations:
-        if shape['attributes'][1]:
-            corners = [shape['points'][:6], shape['points'][2:]]
-            figures = []
-            for corner in corners:
-                start = corner[:2]
-                middle = corner[2:4]
-                end = corner[4:]
-                center = [(start[0] + end[0]) / 2, (start[1] + end[1]) / 2]
-                unknown = [2 * center[0] - middle[0], 2 * center[1] - middle[1]]
-                figures.append([*start, *middle, *end, *unknown])
+        if len(shape['attribute']) == 0:
+            continue
 
-            new_corners = [[*figures[0][:4], *figures[1][6:]],
-                        [*figures[1][4:6], *figures[1][2:4], *figures[0][6:]]]
+        points = [[shape['points'][i], shape['points'][i + 1]]
+                  for i in range(0, len(shape['points']), 2)]
 
-            for corner in new_corners:
-                start = corner[:2]
-                middle = corner[2:4]
-                end = corner[4:]
-                center = [(start[0] + end[0]) / 2, (start[1] + end[1]) / 2]
-                unknown = [2 * center[0] - middle[0], 2 * center[1] - middle[1]]
-                figures.append([*start, *middle, *end, *unknown])
+        if shape['attribute'][0] == '1':
+            ftl, fbl, fbr, rbr = points
+            ftr = calc_fourth_point(ftl, fbl, fbr)
+            rtr = calc_fourth_point(ftr, fbr, rbr)
+            rtl = calc_fourth_point(ftl, ftr, rtr)
+            rbl = calc_fourth_point(rtl, rtr, rbr)
 
-            figures.append([
-                *figures[0][:2], *figures[0][6:], *figures[3][6:], *figures[2][6:]
-            ])
+        elif shape['attribute'][0] == '2':
+            ftr, rtr, rbr, rbl = points
+            fbr = calc_fourth_point(ftr, rtr, rbr)
+            fbl = calc_fourth_point(fbr, rbr, rbl)
+            rtl = calc_fourth_point(rtr, rbr, rbl)
+            ftl = calc_fourth_point(rtl, rtr, ftr)
 
-            if shape['type'] == 'polyline':
-                for figure in figures:
-                    draw.polygon(figure, outline=shape['color'], width=1)
-                draw.line(shape['points'], fill='#3d3df5', width=1)
+        elif shape['attribute'][0] == '3':
+            rtl, rbl, fbl, fbr = points
+            ftl = calc_fourth_point(rtl, rbl, fbl)
+            ftr = calc_fourth_point(ftl, fbl, fbr)
+            rtr = calc_fourth_point(rtl, ftl, ftr)
+            rbr = calc_fourth_point(rtr, ftr, fbr)
+
+        elif shape['attribute'][0] == '4':
+            rtr, rbr, rbl, fbl = points
+            rtl = calc_fourth_point(rtr, rbr, rbl)
+            ftl = calc_fourth_point(rtl, rbl, fbl)
+            ftr = calc_fourth_point(rtr, rtl, ftl)
+            fbr = calc_fourth_point(ftr, ftl, fbl)
+
+        rear = [item for sublist in (rtl, rtr, rbr, rbl) for item in sublist]
+        left = [item for sublist in (ftl, fbl, rbl, rtl) for item in sublist]
+        right = [item for sublist in (ftr, fbr, rbr, rtr) for item in sublist]
+        front = [item for sublist in (ftl, ftr, fbr, fbl) for item in sublist]
+        first_line = [item for sublist in (ftl, fbr) for item in sublist]
+        second_line = [item for sublist in (ftr, fbl) for item in sublist]
+
+        if shape['type'] == 'polyline':
+            draw.polygon(rear, outline=shape['color'], width=line_width)
+            draw.polygon(left, outline=shape['color'], width=line_width)
+            draw.polygon(right, outline=shape['color'], width=line_width)
+            draw.polygon(front, outline=shape['color'], width=line_width)
+            draw.line(shape['points'], fill='#3d3df5', width=line_width)
+            draw.line(first_line, fill='#3d3df5', width=line_width)
+            draw.line(second_line, fill='#3d3df5', width=line_width)
+
     return image
 
 
@@ -128,6 +163,7 @@ app.layout = html.Div([
                   'displayModeBar': True
               }),
     dcc.Store(id='labels', storage_type='session'),
+    dcc.Store(id='attributes', storage_type='session'),
     dcc.Store(id='segments', storage_type='session'),
     dcc.Store(id='job-annotations', storage_type='session')
 ],
@@ -135,19 +171,24 @@ app.layout = html.Div([
 
 
 @app.callback(Output('task-id-dropdown', 'options'),
-              Output('task-id-dropdown', 'value'),
-              Output('labels', 'data'),
+              Output('task-id-dropdown', 'value'), Output('labels', 'data'),
+              Output('attributes', 'data'),
               Input('project-id-dropdown', 'value'))
 def update_task_id_dropdown(selected_project):
     project = client.projects.retrieve(selected_project)
     labels = {label.id: label.to_dict() for label in project.labels}
+    attributes = {}
+    for label in labels.values():
+        for attribute in label['attributes']:
+            if attribute['name'] in ['1', '2', '3', '4']:
+                attributes[attribute['id']] = attribute['name']
 
     tasks = [{
         'label': f'{task.id}: {task.name}',
         'value': task.id
     } for task in sorted(project.get_tasks(), key=lambda x: x.id)]
 
-    return tasks, tasks[0]['value'], labels
+    return tasks, tasks[0]['value'], labels, attributes
 
 
 @app.callback(Output('job-id-dropdown', 'options'),
@@ -201,20 +242,26 @@ def sync_frame_id_inputs(input_value, slider_value):
 
 
 @app.callback(Output('job-annotations', 'data'),
-              Input('job-id-dropdown', 'value'), Input('labels', 'data'))
-def get_job_annotations(job_id, labels):
+              Input('job-id-dropdown', 'value'), Input('labels', 'data'),
+              Input('attributes', 'data'))
+def get_job_annotations(job_id, labels, attributes):
     job = client.jobs.retrieve(job_id)
     job_annotations = job.get_annotations()
     annotations = {}
     for shape in job_annotations.shapes:
-        attributes = [attribute.value for attribute in shape.attributes]
+        attribute = [
+            attributes[str(attribute.spec_id)]
+            for attribute in shape.attributes
+            if (attribute.value == 'true') and
+            (str(attribute.spec_id) in attributes.keys())
+        ]
         frame = shape.frame
         color = labels[str(shape.label_id)]['color']
         points = shape.points
         shape_type = shape.type.value
         id_exists = annotations.get(frame)
         shape_dict = {
-            'attributes': attributes,
+            'attribute': attribute,
             'type': shape_type,
             'color': color,
             'points': points
